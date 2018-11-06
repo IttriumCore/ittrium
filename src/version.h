e8589a88c383120f82c29c88b8f3fb19f940ea4c
Ittrium Cryptocurrency <drew@ittrium.io>
2018-11-06 10:19:26 +0800

    Enforcement before 70900

--- src/version.h
+++ src/version.h
@@ -20,7 +20,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70811;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70900;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT15 = 70811;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT17 = 70900;
 
